9401568e09cf65799b185761e6961f47e36f12a5
Jhon Harold <jhonmaster@gmail.com>
2025-12-11 20:41:41 -0500

    Se actualiza la apiUrl en el frontend

--- parking-frontend/src/environments/environment.prod.ts
+++ parking-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://web-sj1jab1pxp6t.up-de-fra1-k8s-1.apps.run-on-seenode.com'
+  apiUrl: 'https://web-w7f0cwq3bboq.up-de-fra1-k8s-1.apps.run-on-seenode.com'
 };

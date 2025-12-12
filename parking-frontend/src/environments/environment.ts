456ede768781622d5a8f8eac5cd3aafc3d4d9ff1
Jhon Harold <jhonmaster@gmail.com>
2025-12-12 17:06:04 -0500

    feat: Initialize Angular frontend project with basic configuration and development environment.

--- parking-frontend/src/environments/environment.ts
+++ parking-frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  //apiUrl: 'http://localhost:3000'
-  apiUrl: 'https://web-5lg9zpfx9uwa.up-de-fra1-k8s-1.apps.run-on-seenode.com'
+  apiUrl: 'http://localhost:3000'
 };
